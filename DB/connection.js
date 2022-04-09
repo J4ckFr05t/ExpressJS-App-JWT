b305de3ffb1522d77543c6cb8b7ec36ed5c3ba77
Jibin George <jibingeorge1964@gmail.com>
2022-04-09 11:23:40 +0530

    Fixing DB Connection
    Added .env file and updated connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb+srv://jackfrost:Oyp3vN0217tNTY9d@cluster0.omqxx.mongodb.net/htb?retryWrites=true&w=majority'
+const url = 'mongodb+srv://jackfrost:Oyp3vN0217tNTY9d@cluster0.omqxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const connectDB = async ()=>{
     await mongoose.connect(url, {
